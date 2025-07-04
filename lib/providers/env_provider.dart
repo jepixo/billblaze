6406ff6c3c3fbf06ccc8613004c3a9813e515062
jepixo <joelsanjay7@gmail.com>
2025-07-04 17:08:20 +0530

    env variables implemented. layout page UI improved
    google api keys and client ids put into .env

---
+++ lib/providers/env_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final gdriveClientId = dotenv.env['GDRIVE_CLIENT_ID'];
+final gdriveClientSecret = dotenv.env['GDRIVE_CLIENT_SECRET'];
+final googleFontsApiKey = dotenv.env['GFONTS_API_KEY'];
+final gSignInClientId = dotenv.env['GSIGN_IN_CLIENT_ID'];
+final gSignInClientSecret = dotenv.env['GSIGN_IN_CLIENT_SECRET'];
